791a42994a080024fee75b2f4297b7fbae5b2cc4
Yvonne Nkr <yvonnkr86@gmail.com>
2019-08-06 14:41:09 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const name = 'Yvonne Nkr';
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -21,7 +23,6 @@ app.use(express.static(publicDirectoryPath));
 
 //render using handlebars
 
-const name = 'Yvonne Nkr';
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Weather',
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
